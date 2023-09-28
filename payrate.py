a7e6ffb83047b3ad08357749beb9beb31140ad52
RyanAustin <143744224+RyanAustin@users.noreply.github.com>
2023-09-28 12:09:26 -0400

    payrate.py
    in chapter 7 powerpoint

---
+++ payrate.py
@@ -0,0 +1,6 @@
+#hours, payrate,grosspay        
+
+hours = float(input("Enter the number of hours worked"))
+payrate = float(input("Enter the hourly pay rate"))
+gross = hours * payrate
+print(gross)
