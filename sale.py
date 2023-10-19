d598e1de8aeb05ddea5d50de6447f7fe64917128
RyanAustin <143744224+RyanAustin@users.noreply.github.com>
2023-10-19 13:08:37 -0400

    Update sale.py
    putting an array in a for loop and then adding the total of the sale

--- sale.py
+++ sale.py
@@ -2,10 +2,12 @@ sale = []
 
 #for i in sale: 
     
-
+total = 0 
 count = 0
 while count < 6:
-    newSale = input("Please enter a sale:\n")
+    newSale = int(input("Please enter a sale:\n"))
     sale.append(newSale)
     print(sale)
     count += 1 
+    total = total + newSale
+print(total)
