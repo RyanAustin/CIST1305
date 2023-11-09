6cb31aeaaf50ba21a098f6da2c8005497d7dd3c0
RyanAustin <143744224+RyanAustin@users.noreply.github.com>
2023-11-09 13:31:31 -0500

    Create random.py
    Creating and importing a random array by 5

---
+++ random.py
@@ -0,0 +1,9 @@
+import random 
+array = []
+for i in range(5):
+
+    ryan = random.randint(0,24)
+#Had to change the variable to Ryan to have the .append to work in the random
+    array.append(ryan)
+
+print(array)
